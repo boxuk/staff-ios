b673e76706cccdcd64cbf58278fffcc364fc185b
Max Woolf <max.woolf@boxuk.com>
2013-06-21 13:10:14 +0100

    adds working test target, no tests yet though;

---
+++ stafftesting/stafftesting.h
@@ -0,0 +1,13 @@
+//
+//  stafftesting.h
+//  stafftesting
+//
+//  Created by Max Woolf on 21/06/2013.
+//  Copyright (c) 2013 BoxUK. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface stafftesting : SenTestCase
+
+@end
---
+++ stafftesting/stafftesting.m
@@ -0,0 +1,32 @@
+//
+//  stafftesting.m
+//  stafftesting
+//
+//  Created by Max Woolf on 21/06/2013.
+//  Copyright (c) 2013 BoxUK. All rights reserved.
+//
+
+#import "stafftesting.h"
+
+@implementation stafftesting
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in stafftesting");
+}
+
+@end
